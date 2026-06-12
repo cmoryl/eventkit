7a30daffd7c08e00ae0972495542b562ba43958b
cmoryl <cmoryl@gmail.com>
2026-06-12 15:16:29 -0400

    Register expanded brand presets

--- src/brandPresets/index.ts
+++ src/brandPresets/index.ts
@@ -1,13 +1,38 @@
 import { transperfect2026 } from './transperfect2026';
-import { genericBrandPresets, modernSaaS, enterpriseCorporate, hospitality, blankCustomBrand } from './genericPresets';
+import {
+  genericBrandPresets,
+  modernSaaS,
+  enterpriseCorporate,
+  sportsEvent,
+  hospitality,
+  luxuryEvent,
+  nonprofit,
+  creatorBrand,
+  blankCustomBrand
+} from './genericPresets';
 
-export { transperfect2026, genericBrandPresets, modernSaaS, enterpriseCorporate, hospitality, blankCustomBrand };
+export {
+  transperfect2026,
+  genericBrandPresets,
+  modernSaaS,
+  enterpriseCorporate,
+  sportsEvent,
+  hospitality,
+  luxuryEvent,
+  nonprofit,
+  creatorBrand,
+  blankCustomBrand
+};
 
 export const brandPresets = [
   blankCustomBrand,
   modernSaaS,
   enterpriseCorporate,
+  sportsEvent,
   hospitality,
+  luxuryEvent,
+  nonprofit,
+  creatorBrand,
   transperfect2026
 ];
 
