57c98f11eb83bde841b978e18588147a8d2c46aa
cmoryl <cmoryl@gmail.com>
2026-06-12 23:12:08 -0400

    Add narrative panel to intelligence dashboard

--- src/components/powerpoint/PresentationStudioIntelligenceDashboard.tsx
+++ src/components/powerpoint/PresentationStudioIntelligenceDashboard.tsx
@@ -16,6 +16,7 @@ import { PresentationUserFlowPanel } from './PresentationUserFlowPanel';
 import { PresentationFunctionPanel } from './PresentationFunctionPanel';
 import { PresentationSmartBlockPanel } from './PresentationSmartBlockPanel';
 import { PresentationDeckRecipePanel } from './PresentationDeckRecipePanel';
+import { PresentationNarrativePanel } from './PresentationNarrativePanel';
 
 export interface PresentationStudioIntelligenceDashboardProps {
   slides: SlideData[];
@@ -85,6 +86,7 @@ export const PresentationStudioIntelligenceDashboard: React.FC<PresentationStudi
         hasSavedSnapshot={events.some((event) => event.eventType === 'export_created')}
       />
       <PresentationFunctionPanel />
+      <PresentationNarrativePanel />
       <PresentationSmartBlockPanel />
       <PresentationDeckRecipePanel title="Studio deck recipe" goal="Create an editable, brand-safe, export-ready presentation system." />
       <PresentationExportReadinessPanel exportFidelity={state.exportFidelity} agentQA={state.agentQA} allowReviewedProceed={humanApproved} />
